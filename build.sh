9441c5d2fb8c6d21a04f2cc66f18b1bab3d67393
toanlk@bestprice.vn <toanlk@bestprice.vn>
2025-04-17 15:05:48 +0700

    ci: enable docker image tagging and pushing
    This commit uncomments the docker tag and push commands in the build script, enabling the automated tagging and pushing of the docker image to the registry. This change facilitates the continuous deployment process by ensuring the latest image is available for use.

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ clear
 
 docker build -t docker-autopilot .
 
-# docker tag docker-autopilot toanlk/autopilot:latest
-# docker push toanlk/autopilot:latest
+docker tag docker-autopilot toanlk/autopilot:latest
+docker push toanlk/autopilot:latest
