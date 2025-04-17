6426b1fdce30f67620f33b80e66feb94bf32fc6c
toanlk@bestprice.vn <toanlk@bestprice.vn>
2025-04-17 18:10:58 +0700

    refactor(build): simplify build script for non-Linux systems
    The build script was modified to only perform a standard Docker build by default, while multi-platform builds are now conditionally executed only on Linux systems. This change improves the script's usability for developers working on non-Linux platforms.

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 clear
 
-# Use Docker buildx to build multi-platform images
-docker buildx build --platform linux/amd64,linux/arm64 -t docker-autopilot . --push
+docker build -t docker-autopilot .
 
-docker tag docker-autopilot toanlk/autopilot:latest
-docker push toanlk/autopilot:latest
+os_name=$(uname)
+if [ "$os_name" = "Linux" ]; then
+    
+    # Use Docker buildx to build multi-platform images
+    # docker buildx build --platform linux/amd64,linux/arm64 -t docker-autopilot . --push
+
+    # docker tag docker-autopilot toanlk/autopilot:latest
+    # docker push toanlk/autopilot:latest
+
+    echo "Linux build completed."
+fi
