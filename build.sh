22e81a8b1c81d24cf9e725bb5669d825b601aded
Le Khanh Toan <toanlk@bestprice.vn>
2025-05-07 00:01:38 +0700

    Update build

--- build.sh
+++ build.sh
@@ -2,14 +2,29 @@ clear
 
 docker build -t docker-autopilot .
 
-os_name=$(uname)
-if [ "$os_name" = "Linux" ]; then
+platform=$(uname -m)
+if [ "$platform" = "x86_64" ]; then
+    echo "Building for x86_64 architecture..."
+    # docker build -t docker-autopilot .
+
+    os_name=$(uname)
+    echo "Operating System: $os_name"
+
+    if [ "$os_name" = "Darwin" ]; then
+        
+        # Use Docker buildx to build multi-platform images
+        # docker buildx build --platform linux/amd64,linux/arm64 -t docker-autopilot . --push
+
+        docker tag docker-autopilot toanlk/autopilot:latest
+        docker push toanlk/autopilot:latest
+
+        echo "Darwin build completed."
+    fi
     
-    # Use Docker buildx to build multi-platform images
-    # docker buildx build --platform linux/amd64,linux/arm64 -t docker-autopilot . --push
-
-    # docker tag docker-autopilot toanlk/autopilot:latest
-    # docker push toanlk/autopilot:latest
-
-    echo "Linux build completed."
+elif [ "$platform" = "aarch64" ]; then
+    echo "Building for aarch64 architecture..."
+    # docker build -t docker-autopilot .
+else
+    echo "Unsupported architecture: $platform"
+    exit 1
 fi
